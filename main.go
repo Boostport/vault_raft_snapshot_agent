28e6b855f3ac37c699472d01df814afdba9970f6
Francis Chuang <francis.chuang@gmail.com>
2023-07-03 13:03:19 +1000

    Support reloading config using SIGHUP

--- main.go
+++ main.go
@@ -25,39 +25,29 @@ func listenForInterruptSignals() chan bool {
 	return done
 }
 
+func listenForReload() chan os.Signal {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP)
+	return sig
+}
+
 func main() {
 	done := listenForInterruptSignals()
+	reload := listenForReload()
+
+	var (
+		snapshotter         *snapshot_agent.Snapshotter
+		c                   *config.Configuration
+		configuredFrequency time.Duration
+		snapshotTimeout     time.Duration
+		reloadedTimeout     *time.Duration
+	)
 
 	log.Println("Reading configuration...")
-	c, err := config.ReadConfig()
-
-	if err != nil {
-		log.Fatalln("Configuration could not be found")
-	}
-
-	snapshotter, err := snapshot_agent.NewSnapshotter(c)
-	if err != nil {
-		log.Fatalln("Cannot instantiate snapshotter.", err)
-	}
+	snapshotter, c, configuredFrequency, snapshotTimeout = loadConfig()
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
-	configuredFrequency, err := time.ParseDuration(c.Frequency)
-
-	if err != nil {
-		configuredFrequency = time.Hour
-	}
-
-	snapshotTimeout := 60 * time.Second
-
-	if c.SnapshotTimeout != "" {
-		snapshotTimeout, err = time.ParseDuration(c.SnapshotTimeout)
-
-		if err != nil {
-			log.Fatalln("Unable to parse snapshot timeout", err)
-		}
-	}
 
 	var lastSuccessfulUploads snapshot_agent.LastUpload
 
@@ -82,12 +72,16 @@ func main() {
 				}
 			}
 		}
+
 		leader, err := snapshotter.API.Sys().Leader()
+
 		if err != nil {
 			log.Println(err.Error())
 			log.Fatalln("Unable to determine leader instance.  The snapshot agent will only run on the leader node.  Are you running this daemon on a Vault instance?")
 		}
+
 		leaderIsSelf := leader.IsSelf
+
 		if !leaderIsSelf {
 			log.Println("Not running on leader node, skipping.")
 		} else {
@@ -99,13 +93,44 @@ func main() {
 				}
 
 				frequency = lastSuccessfulUploads.NextBackupIn(configuredFrequency)
-			} else {
-				runBackup(ctx, snapshotter, snapshotTimeout)
+
+			} else if reloadedTimeout != nil {
+				frequency = *reloadedTimeout
+				reloadedTimeout = nil
 			}
 		}
+
+		started := time.Now()
+		ends := started.Add(frequency)
+		expires := time.NewTimer(frequency)
+
 		select {
-		case <-time.After(frequency):
-			continue
+		case <-expires.C:
+			if leaderIsSelf {
+				runBackup(ctx, snapshotter, snapshotTimeout)
+			}
+
+		case <-reload:
+			log.Println("Reloading configuration...")
+
+			oldFrequency := configuredFrequency
+
+			snapshotter, c, configuredFrequency, snapshotTimeout = loadConfig()
+
+			if oldFrequency != configuredFrequency {
+				if !expires.Stop() {
+					<-expires.C
+				}
+
+				if started.Add(configuredFrequency).After(ends) {
+					timeout := started.Add(configuredFrequency).Sub(time.Now())
+					reloadedTimeout = &timeout
+				} else {
+					timeout := time.Duration(0)
+					reloadedTimeout = &timeout
+				}
+			}
+
 		case <-done:
 			os.Exit(1)
 		}
@@ -151,4 +176,35 @@ func runBackup(ctx context.Context, snapshotter *snapshot_agent.Snapshotter, sna
 	}
 
 	log.Println("Backup completed.")
+}
+
+func loadConfig() (*snapshot_agent.Snapshotter, *config.Configuration, time.Duration, time.Duration) {
+	c, err := config.ReadConfig()
+
+	if err != nil {
+		log.Fatalln("Configuration could not be found")
+	}
+
+	snapshotter, err := snapshot_agent.NewSnapshotter(c)
+	if err != nil {
+		log.Fatalln("Cannot instantiate snapshotter.", err)
+	}
+
+	configuredFrequency, err := time.ParseDuration(c.Frequency)
+
+	if err != nil {
+		configuredFrequency = time.Hour
+	}
+
+	snapshotTimeout := 60 * time.Second
+
+	if c.SnapshotTimeout != "" {
+		snapshotTimeout, err = time.ParseDuration(c.SnapshotTimeout)
+
+		if err != nil {
+			log.Fatalln("Unable to parse snapshot timeout", err)
+		}
+	}
+
+	return snapshotter, c, configuredFrequency, snapshotTimeout
 }
